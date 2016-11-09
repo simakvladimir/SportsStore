9c2c7ba0278d5919052849f5a389bf7b1b96e1ae
simakvladimir <simakvladimir@gmail.com>
2016-11-09 20:26:42 +0300

    upd: start added admin panel

---
+++ SportsStore.UnitTests/AdminTest.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using SportsStore.Domain.Abstract;
+using SportsStore.Domain.Entities;
+using System.Linq;
+using SportsStore.WebUI.Controllers;
+using System.Collections.Generic;
+
+namespace SportsStore.UnitTests
+{
+    [TestClass]
+    public class AdminTest
+    {
+        [TestMethod]
+        public void Index_Contains_All_Products()
+        {
+            Mock<IProductRepository> mock = new Mock<IProductRepository>();
+            mock.Setup(m => m.Products).Returns(new Product[]
+                {
+                    new Product { ProductID = 1, Name = "P1" },
+                    new Product { ProductID = 2, Name = "P2" },
+                    new Product { ProductID = 3, Name = "P3" }
+                }.AsQueryable());
+
+            AdminController target = new AdminController(mock.Object);
+
+            Product[] result = ((IEnumerable<Product>)target.Index().ViewData.Model).ToArray();
+
+            Assert.AreEqual(result.Length, 3);
+            Assert.AreEqual("P1", result[0].Name);
+            Assert.AreEqual("P2", result[1].Name);
+            Assert.AreEqual("P3", result[2].Name);
+        }
+
+        [TestMethod]
+        public void Can_Edit_Product()
+        {
+            Mock<IProductRepository> mock = new Mock<IProductRepository>();
+            mock.Setup(m => m.Products).Returns(new Product[]{
+                new Product { ProductID = 1, Name = "P1" },
+                new Product { ProductID = 2, Name = "P2" },
+                new Product { ProductID = 3, Name = "P3" },
+            }.AsQueryable());
+
+            AdminController target = new AdminController(mock.Object);
+
+            Product p1 = target.Edit(1).ViewData.Model as Product;
+            Product p2 = target.Edit(2).ViewData.Model as Product;
+            Product p3 = target.Edit(3).ViewData.Model as Product;
+
+            Assert.AreEqual(1, p1.ProductID);
+            Assert.AreEqual(2, p2.ProductID);
+            Assert.AreEqual(3, p3.ProductID);
+        }
+
+        [TestMethod]
+        public void Cannot_Edit_Nonexistent_Product()
+        {
+            Mock<IProductRepository> mock = new Mock<IProductRepository>();
+            mock.Setup(m => m.Products).Returns(new Product[]
+                {
+                    new Product { ProductID = 1, Name = "P1" },
+                    new Product { ProductID = 2, Name = "P2" },
+                    new Product { ProductID = 3, Name = "P3" }
+                }.AsQueryable());
+
+            AdminController target = new AdminController(mock.Object);
+
+            Product result = target.Edit(4).ViewData.Model as Product;
+
+            Assert.IsNull(result);
+        }
+    }
+}
